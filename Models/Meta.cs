90a6d864c61a53eb5be2c909a2cc4de8fecf37ac
Denys Yatsuliak <comdenchik@gmail.com>
2023-02-03 20:22:13 +0200

    fixed build

--- Models/Meta.cs
+++ Models/Meta.cs
@@ -7,15 +7,15 @@ public class Meta
     [JsonProperty("id")]
     public int Result_Count { get; set; }
 
-    [JsonProperty("newest_id")
+    [JsonProperty("newest_id")]
     public string NewestId { get; set; }
 
-    [JsonProperty("oldest_id")
+    [JsonProperty("oldest_id")]
     public string OldestId { get; set; }
 
-    [JsonProperty("next_token")
+    [JsonProperty("next_token")]
     public string NextToken { get; set; }
 
-    [JsonProperty("previous_token")
+    [JsonProperty("previous_token")]
     public string PreviousToken { get; set; }
 }
